99b7c58614ad04a3f543ee37440b97bda231e64f
woong <bsw0312009@cnu.ac.kr>
2020-04-26 21:22:34 +0900

    blog post list (index.)

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Django already made object about User that I have specified as 'author'. I can check in the server/admin
 # admin / 1234
+
 class Post(models.Model):
     title = models.CharField(max_length=30)     # 제목
     content = models.TextField()                # 내용
@@ -10,4 +11,4 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)    # 작성자 ,  on_delete = True : 회원 탈퇴시 django에 연결된 User도 삭제 할 것인지?  3.0 버전에서 True가 아니라 models.CASCADE로 바뀜
 
     def __str__(self):
-        return '{} :: {}'.format(self.title, self.author)
+        return '{} :: {}'.format(self.title, self.author)   # POST라는 객체를 표현될 때 어떻게 표현될 것인지
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request): # request는 기본이므로 건드는 거x
+    posts = Post.objects.all()     # Post에서 모든걸 가져와라 (django 제공)
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
+    
