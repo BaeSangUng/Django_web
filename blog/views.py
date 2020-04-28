ef2caa83e1ef668daf3d3b7d99448541681f7e28
woong <bsw0312009@cnu.ac.kr>
2020-04-28 22:37:28 +0900

    Post: List View

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
+from django.views.generic import ListView
 
-
-def index(request): # request는 기본이므로 건드는 거x
-    posts = Post.objects.all()     # Post에서 모든걸 가져와라 (django 제공)
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-    
+class PostList(ListView):
+    model = Post
+    # created 순서의 반대로
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+# 이 주석이 위에 두줄로 해결(django 기능)
+# def index(request): # request는 기본이므로 건드는 거x
+#     posts = Post.objects.all()     # Post에서 모든걸 가져와라 (django 제공)
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
